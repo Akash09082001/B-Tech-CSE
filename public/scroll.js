a67a2f79f00aa4ef18a3018ebdf43105dd29da49
Akash09082001 <aakashl@letsupgrade.in>
2024-02-14 16:17:32 +0530

    added new change

---
+++ public/scroll.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var elements = document.querySelectorAll(".card-scroll");
+
+    elements.forEach(function (element) {
+        NiceScroll.create(element, {
+            cursorcolor: "#A91D54",
+            cursorwidth: "8px",
+            cursorborder: "none"
+        });
+    });
+});
